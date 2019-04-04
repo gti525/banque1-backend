74bc8f319276b967a72f8cd622319f087ce794db
Jefferson <jeffersonbrutus348@gmail.com>
2019-04-03 21:38:37 -0400

    PATCH

--- src/main/java/com/grokonez/jwtauthentication/controller/PaymentGatewayAPIs.java
+++ src/main/java/com/grokonez/jwtauthentication/controller/PaymentGatewayAPIs.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -94,7 +94,7 @@ public class PaymentGatewayAPIs {
             List<UserCreditCard> results = creditcardRepository.findCreditcard(paymentRequest.getCcNumber(),paymentRequest.getCvv(),paymentRequest.getExpiryDate());
        
          if (results.isEmpty()) {
-            return new ResponseEntity<String>("", HttpStatus.BAD_REQUEST);
+             return new ResponseEntity<Object>(new Response("","DECLINED"), HttpStatus.BAD_REQUEST);
         }
              
             UserCreditCard creditcard = null;
@@ -231,7 +231,7 @@ public class PaymentGatewayAPIs {
 
 
 
-                        return new ResponseEntity<String>(Otherbankresponse, HttpStatus.BAD_REQUEST);
+                        return new ResponseEntity<Object>(new Response("","DECLINED"), HttpStatus.BAD_REQUEST);
 
                     }
                   // Commited Transaction 
